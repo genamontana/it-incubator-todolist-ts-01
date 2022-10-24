2847287734628a50f546fac94c6b77e3bd8f4bae
genamontana <predator199@yandex.ru>
2022-10-24 14:17:06 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList';
+
+export type TaskType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+function App() {
+    //BLL:
+    const todoListTitle: string = 'What to learn'
+    const tasksForTodoList: Array<any> = [
+        {id: 1, title: 'HTML & CSS', isDone: true},
+        {id: 2, title: 'JS & ES6', isDone: true},
+        {id: 3, title: 'React & TS', isDone: false},
+    ]
+
+
+    //GUI:
+    return (
+        <div className="App">
+            <TodoList
+                title={todoListTitle}
+                tasks={tasksForTodoList}
+            />
+        </div>
+    );
+}
+
+export default App;
+
+
